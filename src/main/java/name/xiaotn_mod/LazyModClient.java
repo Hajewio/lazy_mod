8777868d797cefba5b7a4667182a2f1e87e2e205
Hajewio <137734297+Hajewio@users.noreply.github.com>
2023-06-26 13:51:52 +0800

    Add files via upload

---
+++ src/main/java/name/xiaotn_mod/LazyModClient.java
@@ -0,0 +1,10 @@
+package name.xiaotn_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LazyModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
